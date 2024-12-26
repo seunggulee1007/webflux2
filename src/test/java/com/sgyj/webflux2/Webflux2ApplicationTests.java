334c6aefe0d4e90bd56bb6f2b54cccdaa2173f8c
이승구 <leesg107@naver.com>
2024-12-26 23:33:51 +0900

    feat: first commit

---
+++ src/test/java/com/sgyj/webflux2/Webflux2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgyj.webflux2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webflux2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
