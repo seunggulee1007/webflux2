6a846cbcd7ac3881292c0c58df327d638ea83a77
이승구 <leesg107@naver.com>
2024-12-28 13:33:52 +0900

    feat: post api 개발

---
+++ src/main/java/com/sgyj/webflux2/controller/PostController.java
@@ -0,0 +1,20 @@
+package com.sgyj.webflux2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+public class PostController {
+
+    @GetMapping("/{id}")
+    public PostResponse getPostId(@PathVariable(value= "id") Long id) {
+        PostResponse postResponse = new PostResponse();
+        postResponse.setId("1");
+        postResponse.setContent("hello world");
+        return postResponse;
+    }
+
+}
---
+++ src/main/java/com/sgyj/webflux2/controller/PostResponse.java
@@ -0,0 +1,13 @@
+package com.sgyj.webflux2.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostResponse {
+
+    private String id;
+    private String content;
+
+}
